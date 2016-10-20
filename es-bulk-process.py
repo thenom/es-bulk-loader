96f27051627030f069e92f59bbd5d3fd8c58dc9a
Simon Thorley <simon@thenom.co.uk>
2016-10-20 22:02:06 +0100

    V1

--- es-bulk-process.py
+++ es-bulk-process.py
@@ -16,55 +16,52 @@ parser.add_option("-b", "--bulk-size", dest="BulkSize", help="The amount of docu
 parser.add_option("-t", "--index-type", dest="IndexType", help="The type of document in the index")
 parser.add_option("-u", "--user", dest="EsUser", help="Username for the elasticsearch URL")
 parser.add_option("-p", "--pass", dest="EsPass", help="Password for the elasticsearch URL")
-parser.add_option("-d", "--debug", dest="Debug", action="store_true", default=False, help="Enable debug mode, no write out to ES")
 
 (options, args) = parser.parse_args()
 
-if options.Debug == False:
-    if not options.EsHost:
-        print "Elasticsearch host is required"
-        parser.print_help()
-        sys.exit(1)
-    elif not options.BulkCount:
-        print "Run count value is required"
-        parser.print_help()
-        sys.exit(1)
-    elif not options.BulkSize:
-        print "Document bulk size is required"
-        parser.print_help()
-        sys.exit(1)
-    elif not options.FileName:
-        print "Source file is required"
-        parser.print_help()
-        sys.exit(1)
-    elif not options.EsIndex:
-        print "Destination elasticsearch index is required"
-        parser.print_help()
-        sys.exit(1)
-    elif not options.IndexType:
-        print "Destination index type is required"
-        parser.print_help()
-        sys.exit(1)
+if not options.EsHost:
+    print "Elasticsearch host is required"
+    parser.print_help()
+    sys.exit(1)
+elif not options.BulkCount:
+    print "Run count value is required"
+    parser.print_help()
+    sys.exit(1)
+elif not options.BulkSize:
+    print "Document bulk size is required"
+    parser.print_help()
+    sys.exit(1)
+elif not options.FileName:
+    print "Source file is required"
+    parser.print_help()
+    sys.exit(1)
+elif not options.EsIndex:
+    print "Destination elasticsearch index is required"
+    parser.print_help()
+    sys.exit(1)
+elif not options.IndexType:
+    print "Destination index type is required"
+    parser.print_help()
+    sys.exit(1)
 
-    # Setup ES connection
-    es = Elasticsearch([options.EsHost + ':' + options.EsPort], use_ssl=options.SSL)
+# Setup ES connection
+es = Elasticsearch([options.EsHost + ':' + options.EsPort], use_ssl=options.SSL)
 
 # Read in source file
-with open(options.FileName) as f:
+with open(options.FileName, 'r') as f:
     filecontent = f.readlines()
+print 'Read in ' + str(len(filecontent)) + ' lines from file...'
 
 bulk = ''
-
-if options.Debug:
-    print 'File contents:'
-    for fileline in filecontent:
-        print fileline
 
 print 'Processing ' + str(options.BulkCount) + ' bulk posts with ' + str(options.BulkSize) + ' documents in each making a total of ' + str(options.BulkCount * options.BulkSize)
 
 for count in range(0,options.BulkCount):
     for size in range(0,options.BulkSize):
         bulk = bulk + ('{ "' + options.BulkType + '": {}}\n')
-        bulk = bulk + random.choice(filecontent) + '\n'
+        bulk = bulk + random.choice(filecontent).rstrip('\n') + '\n'
+    bulk = bulk.rstrip('\n')
+    es.bulk(body=bulk, index=options.EsIndex, doc_type=options.IndexType)
+    bulk = ''
 
-print bulk
+print "Bulk process complete..."
